e439aed4318e1f4e54a71bf173db1442148f3b44
ChrisN <cneedham@redhat.com>
2019-09-25 10:19:16 +1000

    commit 5

--- app.py
+++ app.py
@@ -2,16 +2,16 @@
 from datetime import datetime
 import socket
 from flask import Flask
+
+
 app = Flask(__name__)
 
-
 @app.route("/")
-    def hello():
-        now = datetime.now()
-        timestamp = now.strftime("%d/%m/%Y %H:%M") 
-        hostname = socket.gethostname()
-        return "%s %s" % (hostname, timestamp)
-
+def hello():
+    now = datetime.now()
+    timestamp = now.strftime("%d/%m/%Y %H:%M") 
+    hostname = socket.gethostname()
+    return "%s %s" % (hostname, timestamp)
 
 if __name__ == "__main__":
     app.run()
--- hello.py
+++
@@ -1,7 +0,0 @@
-# hello.py
-from datetime import datetime
-import socket
-now = datetime.now()
-timestamp = now.strftime("%d/%m/%Y %H:%M") 
-hostname = socket.gethostname()
-print(hostname, timestamp)
