0d82df66f46a6de184fd89e2c8c17612ed12f813
Chris <chris@galah.gateway>
2020-04-09 11:40:36 +1000

    specifiy port

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def hello():
     return "$%s %s %s %s" % ("Hello good world from ", hostname, " at ", timestamp)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='8080')
